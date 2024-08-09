a30c3ba5fd5c176f9ea9cb4fd51da7e45d1de1da
aniruddh <venti.sillly@gmail.com>
2024-08-09 15:06:51 +0530

    update

--- utils/extract.ts
+++ utils/extract.ts
@@ -4,24 +4,32 @@ import Tesseract from 'tesseract.js';
 
 export async function extractTextFromFile(filePath: string, mimeType: string): Promise<string> {
   if (mimeType === 'application/pdf') {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, (err, buffer) => {
-        if (err) {
-          reject(new Error(`Error reading file: ${err.message}`));
-          return;
-        }
-
-        pdf(buffer).then((data: { text: string | PromiseLike<string>; }) => {
-          resolve(data.text);
-        }).catch((error: { message: any; }) => {
-          reject(new Error(`Error parsing PDF: ${error.message}`));
-        });
-      });
-    });
+    return extractTextFromPDF(filePath);
   } else if (mimeType.startsWith('image/')) {
-    const { data: { text } } = await Tesseract.recognize(filePath);
-    return text;
+    return extractTextFromImage(filePath);
   } else {
     throw new Error('Unsupported file type');
   }
+}
+
+async function extractTextFromPDF(filePath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, buffer) => {
+      if (err) {
+        reject(new Error(`Error reading file: ${err.message}`));
+        return;
+      }
+
+      pdf(buffer).then((data: { text: string }) => {
+        resolve(data.text);
+      }).catch((error: { message: any }) => {
+        reject(new Error(`Error parsing PDF: ${error.message}`));
+      });
+    });
+  });
+}
+
+async function extractTextFromImage(filePath: string): Promise<string> {
+  const { data: { text } } = await Tesseract.recognize(filePath);
+  return text;
 }
