4b79d8b02242396d72a26e923ed9a170ec7c6314
aniruddh <venti.sillly@gmail.com>
2024-08-09 13:18:18 +0530

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const path = require('path');
 
-export default nextConfig;
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Ensure WASM files are correctly handled
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/wasm/[hash][ext][query]',
+      },
+    });
+
+    // Resolve the path for the tesseract-core-simd.wasm file
+    config.resolve.alias['tesseract-core-simd'] = path.resolve(__dirname, 'node_modules/tesseract.js-core/tesseract-core-simd.wasm');
+
+    return config;
+  },
+};
